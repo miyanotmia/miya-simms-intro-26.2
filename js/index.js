98676c185746cdac4c3e475e08a6726cb9314137
Miya Simms <miysimms@outlook.com>
2026-05-19 21:43:18 -0400

    js added,createdfooterand skills.

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body');
+const footer = document.createElement("footer");
+
+body.appendChild(footer);
+
+const today = new Date();
+const thisYear = today.getFullYear();
+
+const copyright = document.createElement('p');
+copyright.innerHTML = `\u00a9 Miya Simms ${thisYear}`;
+
+footer.appendChild(copyright);
+
+const skills = ['Sass','MongoDB', 'Vercel', 'Google Web Analytics', 'HTML'];
+const skillsSection = document.querySelector("#Skills");
+
+const skillsList = skillsSection.querySelector("ul");
+
+for (let i = 0; i < skills.length; i++){
+    const skill = document.createElement("li");
+    skill.innerHTML = skills[i];
+    skillsList.appendChild(skill);
+}
